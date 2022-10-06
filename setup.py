ea6a6a2900e564714337982698c839905b43a864
YiHui Liu <liuyihui02@gmail.com>
2022-10-06 20:00:26 +0800

    remove find data files

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
-import os
-from setuptools import setup, find_namespace_packages
-
-
-# the data files
-def data_files():
-    data_file = []
-    data_file_dir = "gdt/data"
-    for root, dirs, files in os.walk(data_file_dir):
-        if files:
-            for file in files:
-                data_file.append(os.path.join(root.replace("gdt/", ""), file))
-    return data_file
+from setuptools import setup
 
 
 def requirement_control():
@@ -27,8 +15,6 @@ setup(
     version=__version__,
     url="https://grid.com",
     packages=["gdt", "gdt.plot", "gdt.utils"],
-    package_data={"gdt.data": data_files()},
-    include_package_data=True,
     license="license.txt",
     author="GRID Team",
     author_email="grid@grid.com",
